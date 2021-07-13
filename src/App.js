e8a337cb8b01d041de9b491adbdc4f850e027f00
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2021-07-13 18:17:58 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Header from "./Header";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
         <div>
           <Switch>
             <Route path="/" exact>
-              This is the homepage
+              <Header />
             </Route>
           </Switch>
         </div>
