ea8b69654df3532b7502cb4f2dc2f9a496619bbf
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2021-07-13 11:02:54 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <h1>Lets build slack</h1>
+      <Router>
+        <div>
+          <Switch>
+            <Route path="/" exact>
+              This is the homepage
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
