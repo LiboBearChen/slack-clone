ae8dcbc911ef30ae6465442a516db37dd9af3bdd
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2021-07-12 20:22:36 -0400

    update

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDsEntGTWn9LBFVVklHHKz5Nxknm0x0-WU",
+  authDomain: "slack-clone-2b250.firebaseapp.com",
+  projectId: "slack-clone-2b250",
+  storageBucket: "slack-clone-2b250.appspot.com",
+  messagingSenderId: "595932805059",
+  appId: "1:595932805059:web:cf6199f0f0dabb0325af76",
+};
